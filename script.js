fdab4cfda657bb8122968e2ff407912597f6d2cc
Vignesh <vigneshsrinivasanoff@gmail.com>
2025-03-22 11:32:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+
+document.querySelector('form').addEventListener('submit', function(e) {
+  e.preventDefault();
+  const name = document.getElementById('name').value;
+  alert(`Thank you, ${name}! Your message has been sent.`);
+});
